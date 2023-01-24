74ac96d188f67e271455d17b87e07bceb52e66cc
VadimK <vadimkuzminskyi@gmail.com>
2023-01-24 18:39:40 +0200

    add

---
+++ script.js
@@ -0,0 +1,19 @@
+// let firstValue = +prompt('введите число');
+// let secondValue = +prompt('ввведите второе');
+// let sum = firstValue + secondValue;
+
+// alert('сумма ' + sum);
+
+// let avg = sum / 2;
+// alert('средннее арифмитическое = ' +avg);
+
+
+const obj = {
+    a: 5
+};
+console.log(obj);
+
+const obj2 = {
+    a: 5
+}
+console.log(obj===obj2);
